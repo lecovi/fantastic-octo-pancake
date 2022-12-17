7b3265df8e969ca471aa5b1ae655dad6dd0660f9
lecovi <lecovi@ac.python.org.ar>
2022-12-17 13:40:05 -0300

    feat: adds LoginForm username and password log submitted info

--- login.py
+++ login.py
@@ -10,13 +10,35 @@ from textual.widgets import (
 
 
 class LoginForm(Container):
+    username = ""
+    password = ""
+
     def compose(self) -> ComposeResult:
         yield Static("Username", classes="label")
-        yield Input(placeholder="Username")
+        yield Input(placeholder="Username", name="username")
         yield Static("Password", classes="label")
-        yield Input(placeholder="Password", password=True)
+        yield Input(placeholder="Password", name="password", password=True)
         yield Static()
         yield Button("Login", variant="primary")
+
+    def on_input_changed(self, event: Input.Changed) -> None:
+        """Event handler called when an input is changed."""
+        if event.input.name == "username":
+            self.username = event.input.value
+            self.log(f"Input username changed: {self.username}")
+        if event.input.name == "password":
+            self.password = event.input.value
+            self.log(f"Input password changed: {self.password}")
+
+    def on_button_pressed(self, event: Button.Pressed) -> None:
+        """Event handler called when a button is pressed."""
+        #TODO: I'm assuming that the input validation will be done here
+        #TODO: I'm assuming that the API request will be done here
+        #      perhaps something like:
+        #
+        #          token = api.login(self.username, self.password)
+
+        self.log(f"Login button pressed ==> U:'{self.username}' P:'{self.password}'")
 
 
 class Pancake(App):
