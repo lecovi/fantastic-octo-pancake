d96a2af1f1b78e65de7c66df0f72bdbcd1267e4f
lecovi <lecovi@ac.python.org.ar>
2022-12-17 14:33:59 -0300

    wip: input validation, sending credentials, CSS

--- login.py
+++ login.py
@@ -1,3 +1,4 @@
+import requests
 from textual.app import App, ComposeResult
 from textual.containers import Container
 from textual.widgets import (
@@ -23,19 +24,35 @@ class LoginForm(Container):
 
     def on_button_pressed(self, event: Button.Pressed) -> None:
         """Event handler called when a button is pressed."""
-        #TODO: I'm assuming that the input validation will be done here
-        #TODO: I'm assuming that the API request will be done here
-        #      perhaps something like:
-        #
-        #          token = api.login(self.username, self.password)
+        if self.is_input_valid():
+            self.log(f"Login button pressed ==> U:'{self.username.value}' P:'{self.password.value}'")
+            self.send_credentials()
 
-        self.log(f"Login button pressed ==> U:'{self.username.value}' P:'{self.password.value}'")
+    def is_input_valid(self):
+        if not self.username.value:
+            self.username.add_class("error")
+            self.log("Username is required")
+            return False
+        if not self.password.value:
+            self.password.add_class("error")
+            self.log("Password is required")
+            return False
+        return True
+
+    def send_credentials(self):
+        #FIXME: Error handling
+        response = requests.post("https://dummyjson.com/auth/login", json={
+            "username": self.username.value,
+            "password": self.password.value,
+        })
+        self.token = response.json()["token"]
+        self.log(f"Sending credentials to API ==> T:'{self.token}'")
 
 
 class Pancake(App):
 
     BINDINGS = [
-        ("q", "quit", "Quit"),
+        ("ctrl+q", "quit", "Quit"),
     ]
 
     def compose(self) -> None:
